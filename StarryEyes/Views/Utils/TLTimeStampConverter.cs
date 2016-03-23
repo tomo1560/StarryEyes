5b5d54484fac0af57ac0f6a09b1370247ceba726
fin-alice <enderalice@yahoo.com>
2016-03-23 15:40:22 +0900

    改造完了 1 ステータスを区切るグリッド線を追加 2 時刻表示に、秒と今日/今月/今年を満たす場合はそれぞれ省いた日付表示を追加 3 アカウントのアイコンを、内容が短い場合は上下を切り取る、長い場合は切り取らないように 4 ぬるぽ対策

---
+++ StarryEyes/Views/Utils/TLTimeStampConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarryEyes.Views.Utils
+{
+	public class TLTimeStampConverter : OneWayConverter<DateTime, string>
+	{
+		protected override string ToTarget(DateTime input, object parameter)
+		{
+			var now = DateTime.UtcNow.Date;
+			if(input >= now)
+			{
+				return input.ToString("HH:mm:ss");
+			}
+			now = now.AddDays(1 - now.Day);
+			if(input >= now)
+			{
+				return input.ToString("dd HH:mm:ss");
+			}
+			now = now.AddMonths(1 - now.Month);
+			if(input >= now)
+			{
+				return input.ToString("MM/dd HH:mm:ss");
+			}
+
+			return input.ToString("\\'yy/MM/dd HH:mm:ss");
+		}
+	}
+}
